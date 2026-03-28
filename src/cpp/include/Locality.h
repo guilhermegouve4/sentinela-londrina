21603201ac8dcf7dd083282e4404d8d4df6434bb
guilhermegouve4 <guilhermepontogouvea@gmail.com>
2026-03-28 16:49:58 -0300

    feat: adiciona WeeklyBulletin e RuralDistrict, atualiza Locality
    - WeeklyBulletin.h/.cpp: estrutura de dados dos boletins semanais com getters
    - RuralDistrict.h: herança de Locality com calculateRisk e isUrban
    - Locality.h: head movido para protected, isUrban() virtual puro, getName() const
    - Locality.cpp: getName() atualizado com const

--- src/cpp/include/Locality.h
+++ src/cpp/include/Locality.h
@@ -6,13 +6,16 @@ class WeeklyBulletin;
 class Locality{
     private:
         std::string m_name;
-        WeeklyBulletin *head;
-
 
     public:
         Locality(std::string name) : m_name(name){}
         virtual double calculateRisk() = 0;
-        std::string getName();
+        std::string getName() const;
         void setName(std::string name);
         virtual ~Locality();
+        virtual bool isUrban() = 0;
+        
+    protected:
+        WeeklyBulletin *head;
+
 };
