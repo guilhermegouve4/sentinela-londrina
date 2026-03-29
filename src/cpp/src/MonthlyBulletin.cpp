0bd7984c7f26d1b4ebe6b7d74d94c64d22d9ffc4
guilhermegouve4 <guilhermepontogouvea@gmail.com>
2026-03-28 21:15:42 -0300

    refactor: renomeia WeeklyBulletin para MonthlyBulletin e week para month

---
+++ src/cpp/src/MonthlyBulletin.cpp
@@ -0,0 +1,13 @@
+#include "../include/MonthlyBulletin.h"
+
+std::string MonthlyBulletin::getMonth() const { return m_month; }
+int MonthlyBulletin::getNotified() const { return m_notified; }
+int MonthlyBulletin::getConfirmed() const { return m_confirmed; }
+int MonthlyBulletin::getDiscarded() const { return m_discarded; }
+int MonthlyBulletin::getUnderAnalysis() const { return m_underAnalysis; }
+int MonthlyBulletin::getDengueCases() const { return m_dengueCases; }
+int MonthlyBulletin::getDengueAlarmCases() const { return m_dengueAlarmCases; }
+int MonthlyBulletin::getDengueSevereCases() const { return m_dengueSevereCases; }
+int MonthlyBulletin::getZikaCases() const { return m_zikaCases; }
+int MonthlyBulletin::getChikungunyaCases() const { return m_chikungunyaCases; }
+int MonthlyBulletin::getDeaths() const { return m_deaths; }
